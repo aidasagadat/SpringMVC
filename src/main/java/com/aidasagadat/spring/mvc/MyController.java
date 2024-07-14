b933b91f6b8563ee32ca9e64bc962d3746712e53
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-14 20:10:58 +0500

    First controller and view

---
+++ src/main/java/com/aidasagadat/spring/mvc/MyController.java
@@ -0,0 +1,26 @@
+package com.aidasagadat.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first-view";
+    }
+
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(){
+        return "ask-employee-details-view";
+    }
+
+
+    @RequestMapping("/showDetails")
+    public String showEmployeeDetails(){
+        return "show-employee-details-view";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>You are doing great!!!</h2>
+
+</body>
+</html>
